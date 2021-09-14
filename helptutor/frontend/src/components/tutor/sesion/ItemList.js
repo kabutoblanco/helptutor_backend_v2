1716ae7b7acebe61f19759f5fe7573d26e8384b1
juan <juanfuentes@unicauca.edu.co>
2021-09-14 17:57:24 -0500

    dashboard sesions

--- helptutor/frontend/src/components/tutor/sesion/ItemList.js
+++ helptutor/frontend/src/components/tutor/sesion/ItemList.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function ItemList(props) {
-    return (
-        <div>
-            {props.data.name}
-        </div>
-    )
+  return (
+    <div className="item-list">
+      <span className='text-truncate'>{props.data.knowledge_area.name}</span>
+      <div className="options">x</div>
+    </div>
+  );
 }
--- helptutor/frontend/src/components/tutor/sesion/List.js
+++ helptutor/frontend/src/components/tutor/sesion/List.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-import ItemList from './ItemList'
+import ItemList from "./ItemList";
+import {tokenConfig} from '../../../redux/actions/utils'
+import store from '../../../redux/store';
 
 export default function List() {
+  const [sesions, setSesions] = useState([]);
+  useEffect(() => {
+    console.log('cargando')
+    axios
+    .get("api/tutor/1/knowledgearea", tokenConfig(store.getState))
+    .then((res) => {
+      setSesions(res.data);
+    })
+    .catch();
+  }, [])
+  
   return (
-    <div className=''>
-      <span className='title-component'>Mis sesiones</span>
-      <ul className='list-group'>
-        <ItemList data={{name: 'sesion1'}} />
+    <div className="">
+      <div className="header-list">
+        <span className="title-component">Mis sesiones</span>
+        <button className="btn btn-primary">AGREGAR</button>
+      </div>
+      <ul className="list-group mt-1">
+        {sesions.map((item) => (
+          <ItemList key={item.id} data={item} />
+        ))}
       </ul>
     </div>
   );
