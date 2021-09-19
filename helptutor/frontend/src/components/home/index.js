5bbd443e4f0f9ef051f249c0a5246c1e550ae82a
mdquilindo <mdquilindo@unicauca.edu.co>
2021-09-18 21:53:22 -0500

    solved router auth

--- helptutor/frontend/src/components/home/index.js
+++ helptutor/frontend/src/components/home/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { useHistory } from 'react-router-dom';
 
 export default function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  const history = useHistory();
+
+  const auth = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      history.push('/rol');
+    }
+  }, []);
+
+  return <div>Home</div>;
 }
