14d05029ca805027a6864a13b7d6dda1bd578ba1
mdquilindo <mdquilindo@unicauca.edu.co>
2021-09-30 23:34:58 -0500

    refactor tutor offer views

--- helptutor/services/serializers/offer.py
+++ helptutor/services/serializers/offer.py
@@ -22,15 +22,21 @@ class OfferViewSerializer(serializers.ModelSerializer):
 class OfferViewCustomSerializer(serializers.Serializer):
     id = serializers.SerializerMethodField()
     title = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
     description = serializers.SerializerMethodField()
     student = serializers.SerializerMethodField()
     tutor_id = serializers.SerializerMethodField()
+    date_record = serializers.SerializerMethodField()
+    date_update = serializers.SerializerMethodField()
 
     def get_id(self, obj):
         return obj[0]
 
     def get_title(self, obj):
         return obj[4]
+
+    def get_price(self, obj):
+        return obj[17]
 
     def get_description(self, obj):
         return obj[5]
@@ -42,3 +48,8 @@ class OfferViewCustomSerializer(serializers.Serializer):
     def get_tutor_id(self, obj):
         return obj[9]
 
+    def get_date_record(self, obj):
+        return obj[2]
+
+    def get_date_update(self, obj):
+        return obj[3]
